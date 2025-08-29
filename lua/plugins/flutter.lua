9b35429b89cd055ecb0df1ae7d3e6210f015b024
jack8096 <95523743+jack8096@users.noreply.github.com>
2025-08-29 21:24:30 +0530

    Update flutter.lua

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -1,29 +1,18 @@
 return {
-  "akinsho/flutter-tools.nvim",
-  ft = "dart",
-  lazy = true,
+  "nvim-flutter/flutter-tools.nvim",
+  lazy = false,
   dependencies = {
     "nvim-lua/plenary.nvim",
     "stevearc/dressing.nvim", -- optional for vim.ui.select
+    "nvim-telescope/telescope.nvim",
   },
-  -- opts = {
-  --   widget_guides = {
-  --     enabled = true,
-  --   },
-  --   lsp = {
-  --     color = {
-  --       enabled = true,
-  --     },
-  --     on_attach = function() end,
-  --   },
-  -- },
   config = function()
     require("telescope").load_extension("flutter")
     vim.keymap.set(
       "n",
       "<leader>a",
       require("telescope").extensions.flutter.commands,
-      { noremap = true, silent = false }
+      { noremap = true, desc = "Commands", silent = false }
     )
     require("flutter-tools").setup({
       widget_guides = {
@@ -36,14 +25,5 @@ return {
         on_attach = function() end,
       },
     })
-
-    -- require("flutter-tools").setup_project({
-    --   {
-    --     name = "Custom launch configs for flutter",
-    --     flavor = "webFlavor",
-    --     flutter_mode = "debug",
-    --     dart_define_from_file = "launch.json",
-    --   },
-    -- })
   end,
 }
